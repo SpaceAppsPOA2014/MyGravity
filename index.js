fd3a59432a5f73005efdb4fdb83d8a5638c1fd78
guifroes <guilherme.froes@gmail.com>
2014-04-13 00:01:08 -0300

    testing heroku again

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/geoid/:lat/:lng', function(req, res){
 	});
 });
 
-var server = app.listen(3000, function() {
+app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
